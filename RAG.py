a2cacd683b7068527e3f735e0cacc2fba0965e2b
Fynn1612 <f.schueder95@gmx.de>
2025-09-01 10:05:33 +0200

    Refactor code structure for improved readability and maintainability

--- RAG.py
+++ RAG.py
@@ -1,4 +1,3 @@
-#from pydoc import doc
 from langchain_community.document_loaders import UnstructuredPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
@@ -9,13 +8,12 @@ from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
 import os
 
-from utils.config import GOOGLE_API_KEY, GROQ_API_KEY
+from utils.config import GOOGLE_API_KEY
 
 #from langchain.chains import create_retrieval_chain
 #from langchain.chains.combine_documents import create_stuff_documents_chain
 
 from langchain_google_genai import ChatGoogleGenerativeAI
-#from langchain_groq import ChatGroq
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import streamlit as st
 
@@ -32,7 +30,13 @@ GOOGLE_DRIVE_BASE = r"G:\Meine Ablage"
 folder_path = PDF_FOLDER
 
 llm_model_name = "gemini-2.5-flash"
+
 def get_embedding():
+    """Get the embedding model instance.
+
+    Returns:
+        GoogleGenerativeAIEmbeddings: The embedding model instance.
+    """
     import asyncio
     try:
         asyncio.get_running_loop()
@@ -44,6 +48,11 @@ def get_embedding():
 
 @st.cache_resource
 def load_pdf():
+    """Load PDF documents from the specified folder and extract metadata.
+    
+    Returns:
+        tuple: A tuple containing the current metadata, if there were changes and a list of loaded documents.
+    """
 
     current_metadata = get_pdf_metadata(folder_path)
     stored_metadata = load_stored_metadata(METADATA_PATH)
@@ -53,13 +62,13 @@ def load_pdf():
         documents = []
         # find new PDFs
         new_pdfs = []
-        print(f"Current metadata: {current_metadata}")
-        print(f"Stored metadata: {stored_metadata}")
+        #print(f"Current metadata: {current_metadata}")
+        #print(f"Stored metadata: {stored_metadata}")
 
         for  file_name,  last_modified in current_metadata.items():
             # last_modified = meta["last_modified"]
-            print(file_name)
-            print(last_modified)
+            #print(file_name)
+            #print(last_modified)
             if file_name not in stored_metadata:
                 new_pdfs.append(file_name)
                 print(f"[DEBUG] {file_name} ist NEU (nicht in stored_metadata).")
@@ -84,7 +93,22 @@ def load_pdf():
 
 @st.cache_resource
 def _build_rag_chain(documents, llm_model_name = llm_model_name, changes=False, current_metadata=None):
+    """
+    
+
+    Args:
+        documents (list): List of newly uploaded or changed documents to be processed.
+        llm_model_name (str): Name of the LLM model to use for the RAG chain.
+        changes (bool): Flag indicating if there are new or updated documents.
+        current_metadata (dict): Metadata of the current set of documents.
+
+    Returns:
+        rag_chain: The RAG chain for document retrieval and question answering.
+    """
+    
+    # Get embedding model
     embedding = get_embedding()
+    
     if changes:
         splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
         
@@ -150,32 +174,48 @@ def _build_rag_chain(documents, llm_model_name = llm_model_name, changes=False, 
         | StrOutputParser()
     )
 
-    # # === Example Query ===
-    # user_question = "What is epistemic uncertainty?"
-    # response = rag_chain.invoke(user_question)
-
-    # print("\n=== RAG Response ===\n")
-    # print(response)
     return rag_chain
 
 def main():
-    st.title("Masterthesis PDF Quotes RAG")
+    st.set_page_config(page_title="Masterthesis Literature Chatbot", layout="centered")
+    st.title("Masterthesis Literature Chatbot")
+    st.caption("Chat with multiple PDFs :books:")
+    
+    # Initialize required Streamlit session state variables
+    setup_session_state()
+    
+    # Allow user to download the chat history as CSV
+    if st.session_state.chat_history:
+        render_download_chat_history()
+        
+    # Render previous chat messages (Q&A)
+    if st.session_state.get("chat_history", []):
+        render_chat_history()
 
+    # Load new or updated PDFs
     changes, current_metadata, documents = load_pdf()
     if not documents:
         st.info("No new or updated PDFs found. Using existing vectorstore.")
-        # changes = False
-        # current_metadata = load_stored_metadata(METADATA_PATH)
 
+    # Build RAG chain
     rag_chain = _build_rag_chain(documents, llm_model_name=llm_model_name, changes=changes, current_metadata=current_metadata)
 
-    question = st.text_input("Ask a question about the PDFs:")
-    if question:
+    # Create a text input for user questions
+    question = st.chat_input("Ask a question about the PDFs:")
+    if not question:
+        return
+    
+    with st.chat_message("user"):
+        st.markdown(f"**You:** {question}")
+    with st.chat_message("ai"):
+        
         with st.spinner("Thinking..."):
             try:
                 response = rag_chain.invoke(question)
                 st.markdown("### Answer")
                 st.success(response)
+                st.session_state.chat_history.append({"question": question, "answer": response})
+        
             except Exception as e:
                 st.error(f"Error processing your question: {e}")
 
